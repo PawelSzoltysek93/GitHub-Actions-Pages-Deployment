a802c91ec27a7dd9e9f017e72f8acb4b15ce7e81
PawelSzoltysek93 <pawel.szoltysek@dci-student.org>
2025-10-22 11:17:14 +0200

    Hero and About sections created

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,23 @@
 import { Navbar } from "../components/Navbar";
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
       <ThemeToggle />
+      {/* Background in dark mode */}
       <StarBackground />
+      {/* Responsive NavBar */}
       <Navbar />
-
       {/* Main Content */}
-
+      <main>
+        <HeroSection />
+        <AboutSection />
+      </main>
       {/* Footer */}
     </div>
   );
